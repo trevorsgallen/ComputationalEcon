f362ec9352471344ffbf2c9696856bae25953ffe
Trevor S. Gallen <41443675+trevorsgallen@users.noreply.github.com>
2023-11-03 10:00:57 -0500

    Homework 1 - Embedding Figures for Legibility
    I embedded the figures into Homework 1 for legibility.

--- Homework 1/Main_Hwk1_Sol.m
+++ Homework 1/Main_Hwk1_Sol.m
@@ -14,6 +14,7 @@ clear
     V_1 = zeros(size(h_space));
     i_policy = 0.5*ones(size(h_space));
 
+%VFI
 error = Inf;
 while error > 1e-10
     for h_ind = 1:length(h_space)
@@ -25,10 +26,12 @@ while error > 1e-10
 
         %Minimize the negative, start at best policy from last iteration
         [temp1,temp2]=fmincon(@(i)-ut(i),i_policy(h_ind),[],[],[],[],0.0001,0.9999,[],optimset('Display','off'));
-
+        
+        %Store Value and Policy Functions
         V_1(h_ind) = -temp2;
         i_policy(h_ind) = temp1;
     end
+    %After looping over all states, find error and create new value fxn
     error = max(abs(V_1-V_0))
     V_0 = V_1;
 end
@@ -39,10 +42,14 @@ end
     xlabel('h')
     ylabel('i')
     title('Policy Function')
+    saveas(gcf,'Fig1.png') 
 
 %Simulate
-    h_sim = random('uniform',1,15,100,1);
-    for t = 1:40
+    N = 100;
+    T = 40;
+
+    h_sim = random('uniform',1,15,N,1);
+    for t = 1:T
         i_sim(:,t)=interp1(h_space,i_policy,h_sim(:,t));
         h_sim(:,t+1)=(1-delta)*h_sim(:,t)+A.*i_sim(:,t).^gamma;
     end
@@ -52,6 +59,7 @@ end
     xlabel('t')
     ylabel('h')
     title('Simulated Paths of Human Capital')
+    saveas(gcf,'Fig2.png') 
 
 %Find steady state human capital using fzero
     hstar = fzero(@(h) (1-delta)*h+A.*interp1(h_space,i_policy,h).^gamma - h,6);
