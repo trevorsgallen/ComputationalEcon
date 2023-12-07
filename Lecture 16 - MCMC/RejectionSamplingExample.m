b6a4240f7cb8bb561093e7e7a683346422435d53
Trevor S. Gallen <41443675+trevorsgallen@users.noreply.github.com>
2023-12-07 16:52:46 -0500

    Update MCMC slides with rejection sampling description, example
    Update MCMC slides with rejection sampling description, example

--- Lecture 16 - MCMC/RejectionSamplingExample.m
+++ Lecture 16 - MCMC/RejectionSamplingExample.m
@@ -4,20 +4,21 @@ mu = 0;
 sigma = 5;
 
 x_vec = -20:0.1:20
-f = pdf('Normal',mu,sigma,x_vec)
+f = pdf('Normal',x_vec,mu,sigma)
 
 Urand_x = random('Uniform',-20,20,10000,1);
-Urand_y = random('Uniform',1,10000,1);
+Urand_y = random('Uniform',0,0.2,10000,1);
 
-insample=Urand_y<=pdf('Normal',0,5,Urand_x)
+insample=Urand_y<=pdf('Normal',Urand_x,0,5)
 
 plot(x_vec,f,'-k')
 hold on
-scatter(Urand_x(insample==1),Urand_y(insample==1),'-r')
+scatter(Urand_x(insample==1),Urand_y(insample==1),'r')
 hold on
-scatter(Urand_x(insample==1),Urand_y(insample==1),'-b')
+scatter(Urand_x(insample==0),Urand_y(insample==0),'b')
 title('Rejection Sampling')
-xtitle('x')
-ytitle('Density')
+xlabel('x')
+ylabel('Density')
+legend('pdf','Sampled','Rejected')
 
 
